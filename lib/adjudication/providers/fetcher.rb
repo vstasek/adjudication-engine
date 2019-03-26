0495766405f64bb1636586d136ce29a498cfc734
Victor Stasek <stasekvictordev@gmail.com>
2019-03-25 23:34:28 -0400

    adds logic to manipulate provider data so its in the same form as claims data

--- lib/adjudication/providers/fetcher.rb
+++ lib/adjudication/providers/fetcher.rb
@@ -15,7 +15,14 @@ module Adjudication
           raise "Error fetching Provider data - HTTP Code: " + response.code.to_s
         end
 
-        prov_data
+        # Put provider data in same format as claims data for easy comparison
+        # Create array containing CSV header columns
+        fields = Array.new
+        prov_data[0].map { |header_col| fields << header_col}
+        prov_data.shift # Remove row containing CSV header cols from provider data
+
+        # Convert Provider prov_data to array of hashes; (same format as claims_data)
+        prov_data.map { |row| fields.zip(row).to_h }
       end
 
       def filter_data prov_data
