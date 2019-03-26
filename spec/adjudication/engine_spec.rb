cba14ace9dc43f147075dc1c34cc6946a483a27d
Victor Stasek <stasekvictordev@gmail.com>
2019-03-25 23:06:13 -0400

    adds pending examples for testing adjudicator

--- spec/adjudication/engine_spec.rb
+++ spec/adjudication/engine_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Adjudication::Engine do
   it "has a version number" do
     expect(Adjudication::Engine::VERSION).not_to be nil
   end
+
+  describe Adjudication::Engine::Adjudicator do
+    it "determines out of network claims"
+    it "rejects out of network claims"
+    it "rejects duplicate claims"
+    it "pays valid line items"
+    it "rejects invalid line items"
+  end
 end
