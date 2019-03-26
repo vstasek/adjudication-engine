66447077d22a88841960d2399342ccb5238408f0
Victor Stasek <stasekvictor@gmail.com>
2019-03-26 12:09:04 -0400

    adds test for determining if a claim is in network

--- spec/adjudication/engine_spec.rb
+++ spec/adjudication/engine_spec.rb
@@ -6,7 +6,30 @@ RSpec.describe Adjudication::Engine do
   end
 
   describe Adjudication::Engine::Adjudicator do
-    it "determines out of network claims"
+    it "determines out of network claims" do
+      claims_data = [
+      {"npi" => "1234567890"}, # In Network
+      {"npi" => "1770797672"}, # Out Network
+      {"npi" => "6666666666"}, # Out
+      {"NoNPI" => ""}, # N/A
+      {"npi" => "1112223334", "secondKey" => "abc"} # In
+      ]
+
+      prov_data = [
+      {"NPI" => "1234567890"}, # Match
+      {"NPI" => "1112223334"}, # Match
+      {"NPI" => "5555555555"}, # No Match
+      {"NPI" => "9517534567"}, # No Match
+      {"NPI" => "0007774203"}, # No Match
+      ]
+
+      adjudicator = Adjudication::Engine::Adjudicator.new
+      adjudicator.matchNPI(claims_data, prov_data)
+      how_many  = claims_data.count { |claim| claim["in_network"] == true }
+
+      expect(how_many).to be 2
+    end
+
     it "rejects out of network claims"
     it "rejects duplicate claims"
     it "pays valid line items"
