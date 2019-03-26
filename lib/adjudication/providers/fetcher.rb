824a7a55015dfb66f8611c79333d26762bc9921a
Victor Stasek <stasekvictordev@gmail.com>
2019-03-25 23:04:55 -0400

    implements provider_data - successfully fetches csv provider data

--- lib/adjudication/providers/fetcher.rb
+++ lib/adjudication/providers/fetcher.rb
@@ -1,17 +1,24 @@
-#require 'net/http'
 require 'httparty'
-
 
 module Adjudication
   module Providers
     class Fetcher
       def provider_data
-        # STEP 1: Import CSV data and return it.
+        # STEP 1: Import Provider CSV data and return it.
 
         url = 'http://provider-data.beam.dental/beam-network.csv'
         response = HTTParty.get(url)
-        #puts response.parsed_response
 
+        if response.code.to_i == 200 # HTTP Response Code
+          prov_data = response.parsed_response # nested arrays
+        else
+          raise "Error fetching Provider data - HTTP Code: " + response.code.to_s
+        end
+
+        prov_data
+      end
+
+      def filter_data prov_data
       end
     end
   end
